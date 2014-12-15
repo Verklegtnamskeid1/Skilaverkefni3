78f0a8beef3689ee17d11cbff20acb01f242d64d
ingthorb <ingthorb@gmail.com>
2014-12-15 18:13:08 +0000

    Update insertcomputer.cpp

--- insertcomputer.cpp
+++ insertcomputer.cpp
@@ -6,6 +6,10 @@ Insertcomputer::Insertcomputer(QWidget *parent) :
     ui(new Ui::Insertcomputer)
 {
     ui->setupUi(this);
+    ui->lineEdit_namecomputer->setPlaceholderText("For example skynet");
+    ui->lineEdit_type->setPlaceholderText("Terminator is a type");
+    ui->lineEdit_wasitbuilt->setPlaceholderText("Yes or no question");
+    ui->lineEdit_yearbuilt->setPlaceholderText("Btw skynet has not been built");
 }
 
 Insertcomputer::~Insertcomputer()
